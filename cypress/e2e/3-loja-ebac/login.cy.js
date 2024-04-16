1d8883cf22518bcacc9c99afffe7d769ffbfcc1d
Marcos Colossi <colossimarcos34@gmail.com>
2024-04-16 11:46:22 -0300

    validando mensagens de erro e o uso de hoots beforeEac para visit e afterEach para screenshot

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,19 +1,36 @@
 /// <reference types="cypress" />
 
 describe('Verifica se o ecommerce Ebac-Shop está online', () => {
-    it('Visualza a tela de login', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    afterEach(() => {
+        cy.screenshot()
+    });
+    
+    it('Visualiza a tela de login', () => {
         cy.get('#customer_login > :nth-child(1)').should('contain', 'Username or email address *')
     })  
     
     it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('gaviao.arqueiro@teste.com.br')
         cy.get('#password').type('Qa12345678')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('a > .hidden-xs').should('contain', 'Welcome gaviao.arqueiro !')
-    }) 
+    })
+    
+    it('Deve visualizar mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('gaviao.arqueiro@teste.com')
+        cy.get('#password').type('Qa12345678')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido.')
+    })
 
+    it.only('Deve visualizar mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('gaviao.arqueiro@teste.com.br')
+        cy.get('#password').type('Qa0000000')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail gaviao.arqueiro@teste.com.br está incorreta.')
+    })
 
 })
--- cypress/e2e/3-loja-ebac/online.cy.js
+++ cypress/e2e/3-loja-ebac/online.cy.js
@@ -1,33 +1,34 @@
 /// <reference  types="cypress" /> 
 
 describe('Ebac-shop deve estar online', () => {
-    // Verifica se a loja está no ar     
+   beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+   });   
+
+   afterEach(() => {
+    cy.screenshot()
+   });
+
     it('acessando a pagina de login', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#tbay-topbar').should('contain', 'Sign up')
     })  
 })
 
 describe('Registro na Ebac-Shop', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')        
+    });
+    afterEach(() => {
+        cy.screenshot()
+    });
 
-    it('deve poder registrar novo usuário na plataforma', () => {
-        // Acessa a tela de login, e garante que o usuário tenha a possiblilidade de registrar novo usuário
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it.only('deve poder registrar novo usuário na plataforma', () => {
         cy.get(':nth-child(2) > h2').should('contain', 'Register')
-
-        // Insere usuário e senha e clica no botão para registrar novo usuário
-        cy.get('#reg_email').type('gaviao.arqueiro@teste.com.br')
+        cy.get('#reg_email').type('nebulosa@teste.com.br')
         cy.get('#reg_password').type('Qa12345678')
         cy.get(':nth-child(4) > .button').click()
-        
-        // Verifica se o usuário foi registrado com sucesso via mensagem de boas vindas
-        cy.contains('gaviao.arqueiro !').should('exist')       
-        
-        // Faz logout da aplicação validando via mensagem de texto na página.
+        cy.contains('nebulosa !').should('exist')             
         cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').click()
-        cy.get('.products-carousel-thumbnail > .widget-title > span').should('contain', 'PRODUTOS SUGERIDOS') // Não encontra o elemento  
-        
-
+        cy.get('.vc_custom_1503911295487').should('contain', 'PRODUTOS SUGERIDOS')  
     })
-
 })
